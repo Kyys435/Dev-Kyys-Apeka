cbd35f04c0a3f71015dc4ea1a58c5ce3eab88025
Kyys435 <lusisuryani2010@gmail.com>
2025-12-10 15:15:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+/* ============================================
+   Dev Kyys Apeka
+   External JavaScript Files
+   ============================================ */
+
+// Google Tag Manager
+// https://www.googletagmanager.com/gtag/js?id=G-8RCQ4JFZ27
+
+// Google Adsense
+// https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js
+
+// VPS JavaScript (Main)
+– https://www.view-page-source.com/j/z.js?ver=1.0
+
+// VPS Rendering Parser
+// https://www.view-page-source.com/j/zrp.js
+
+// VPS Script (footer functions)
+// https://www.view-page-source.com/j/s.js?ver=1.0
+
+// Prism Syntax Highlighter (core)
+// https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js
+
+// Prism Markup Component
+// https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-markup.min.js
+
+
+
+/* ============================================
+   Inline JavaScript
+   ============================================ */
+
+/* Schema JSON-LD */
+{
+    "@context": "http://schema.org/",
+    "@type": "WebSite",
+    "@id": "#website",
+    "name": "Kyys Apeka No Limit",
+    "alternateName": "Kyys Apeka No Limit",
+    "url": "https://www.kyys.apeka.com/"
+}
+
+
+/* Google Analytics Init */
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-8RCQ4JFZ27');
+
+
+/* Google Adsense Load */
+(adsbygoogle = window.adsbygoogle || []).push({});
